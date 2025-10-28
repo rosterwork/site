bd9df979960eb60f2c1bd59b42d8c127dace3403
rosterwork <rosterwork.wfm@gmail.com>
2025-10-28 19:35:09 -0300

    Update URLGoogleScript.js

--- js/URLGoogleScript.js
+++ js/URLGoogleScript.js
@@ -1,4 +1,5 @@
-const URL_GOOGLE_SCRIPT = 'https://script.google.com/macros/s/AKfycbyvujnmbZX8S_G7tRsL4JqR1BteB_JIjOlAF0xOLVXJA__zs9TLlG00McbTJIFHexOk/exec';
+const URL_GOOGLE_SCRIPT = 'https://script.google.com/macros/s/AKfycbwR_3c7oz6-FLAFIK9u6DiB8WUpSndY3Y8Afokc12axAzXWMWYHfm5ppJkaaXN6WALH/exec';
+
 
 
 
