633f8dfae4ff744529a4c2ae6a44f9004f8c03b9
rosterwork <rosterwork.wfm@gmail.com>
2025-11-04 11:11:05 -0300

    Update URLGoogleScript.js

--- js/URLGoogleScript.js
+++ js/URLGoogleScript.js
@@ -1,1 +1,1 @@
-const URL_GOOGLE_SCRIPT = 'https://script.google.com/macros/s/AKfycbzPMjqcJnA9cXT1OVpJQs64lEFXyH-4m40JAp5gHzGVUkZj9AsWT_kkJ1zXHM-mT6L-/exec';
+const URL_GOOGLE_SCRIPT = 'https://github.com/rosterwork/action/actions/workflows/deploy.yml/dispatches';
